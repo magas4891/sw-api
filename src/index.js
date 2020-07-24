7fe5912fe80a6f1f82eb2ae474efba5afad2359c
magas4891 <magasishe@gmail.com>
2020-07-24 18:07:03 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,57 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app";
 
-
-class SwapiService {
-
-    _apiBase = 'https://swapi.dev/api';
-
-    async getResourse(url) {
-        const res = await fetch(`${this._apiBase}${url}`);
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, received ${res.status}`)
-        }
-        return await res.json();
-    }
-
-    async getAllPeople() {
-        const peopleArray = await this.getResourse(`/people/`);
-        return peopleArray.results;
-    }
-    async getPerson(id) {
-        return this.getResourse(`/people/${id}`);
-    }
-    async getAllPlanets() {
-        const planetArray = await this.getResourse(`/planets/`);
-        return planetArray.results;
-    }
-    async getPlanet(id) {
-        return this.getResourse(`/planets/${id}`);
-    }
-    async getAllStarships() {
-        const starshipsArray = await this.getResourse(`/starships/`);
-        return starshipsArray.results;
-    }
-    async getStarship(id) {
-        return this.getResourse(`/starships/${id}`);
-    }
-    async getAllFilms() {
-        const filmsArray = await this.getResourse(`/films/`);
-        return filmsArray.results;
-    }
-    async getFilms(id) {
-        return this.getResourse(`/films/${id}`);
-    }
-    async getAllSpecies() {
-        const speciesArray = await this.getResourse(`/species/`);
-        return speciesArray.results;
-    }
-    async getSpecie(id) {
-        return this.getResourse(`/species/${id}`);
-    }
-    async getAllVehicles() {
-        const vehiclesArray = await this.getResourse(`/vehicles/`);
-        return vehiclesArray.results;
-    }
-    async getVehicle(id) {
-        return this.getResourse(`/vehicles/${id}`);
-    }
-}
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
